effbd46dcb77a112b373555c8189fc6a6c02f41e
Michael <109226084+fishtrap2@users.noreply.github.com>
2026-03-19 13:41:11 -0400

    Update els.py
    fixed filename

--- src/els.py
+++ src/els.py
@@ -3,6 +3,6 @@ import os
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
 def load_els_model():
-    path = os.path.join(BASE_DIR, "src/schemas", "els_model.yaml")
+    path = os.path.join(BASE_DIR, "src/schemas", "els_schema.yaml")
     with open(path, "r") as f:
         return yaml.safe_load(f)
